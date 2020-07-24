228befa8a0f7b70ace177c4616fc4ccc62a711fc
Alessio Morale <alessiomorale@gmail.com>
2020-07-24 20:18:25 +0100

    [OP/REVO] - fix serial ports/timer dma

--- boards/op/revolution/default.cmake
+++ boards/op/revolution/default.cmake
@@ -14,34 +14,15 @@ px4_add_board(
 
 	DRIVERS
 		adc
-		#barometer # all available barometer drivers
 		barometer/ms5611
-		#batt_smbus
-		#camera_trigger
-		#differential_pressure # all available differential pressure drivers
-		#distance_sensor # all available distance sensor drivers
 		dshot
 		gps
 		imu/invensense/mpu6000
-		#irlock
-		#lights/blinkm
-		#lights/rgbled
-		#magnetometer # all available magnetometer drivers
 		magnetometer/hmc5883
-		#mkblctrl
-		#optical_flow/px4flow
-		#osd
-		#pca9685
-		#pwm_input
-		#pwm_out_sim
 		pwm_out
 		rc_input
-		#tap_esc
-		#telemetry # all available telemetry drivers
 		telemetry/frsky_telemetry
-		#test_ppm
 	MODULES
-		#airspeed_selector
 		attitude_estimator_q
 		battery_status
 		#camera_feedback
@@ -49,12 +30,10 @@ px4_add_board(
 		dataman
 		ekf2
 		events
-		#fw_att_control
-		#fw_pos_control_l1
 		land_detector
-		#landing_target_estimator
+		landing_target_estimator
 		load_mon
-		#local_position_estimator
+		local_position_estimator
 		logger
 		mavlink
 		mc_att_control
@@ -63,12 +42,8 @@ px4_add_board(
 		mc_rate_control
 		navigator
 		rc_update
-		#rover_pos_control
 		sensors
-		#sih
 		#temperature_compensation
-		#vmount
-		#vtol_att_control
 	SYSTEMCMDS
 		#bl_update
 		dmesg
@@ -87,22 +62,10 @@ px4_add_board(
 		pwm
 		reboot
 		reflect
-		#sd_bench
-		#tests # tests and test runner
 		top
 		#topic_listener
 		tune_control
 		usb_connected
 		ver
 		work_queue
-	EXAMPLES
-		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		#hello
-		#hwtest # Hardware test
-		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		#rover_steering_control # Rover example app
-		#uuv_example_app
-		#work_item
 	)
